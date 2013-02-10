590d69c44c6f93a3800b382b4bc1c45cbd7e20cb
nbhartiya <neeharika@alum.mit.edu>
2013-02-10 14:18:07 -0800

    generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MVP::Application.config.secret_token = 'ed44931eee354e521c94dee0986ffa319d1cf6e585b7d5ccc7919000639f512aaaae33462742c66812c03fd35193c686d7325b0ef8399b0326dfdaca02743c00'
