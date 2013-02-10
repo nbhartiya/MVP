1545b7abd1d76a3edc45f6ac39c29bf3a6155e27
nbhartiya <neeharika@alum.mit.edu>
2013-02-10 15:13:02 -0800

    set up devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ MVP::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # added for devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ MVP::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'user#index'
+  root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
