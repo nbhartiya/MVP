d9f406ded02726abd290429ab038c4ec30f1e6da
nbhartiya <neeharika@alum.mit.edu>
2013-02-12 18:45:49 -0800

    change type to kind in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,5 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :approved, :first_name, :last_name, :type
+  attr_accessible :approved, :first_name, :last_name, :kind
 end
