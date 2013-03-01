8119bbb95312cbe7f72697200b4f9ee1be3850b5
nbhartiya <neeharika@alum.mit.edu>
2013-03-01 14:54:56 -0800

    made one method for all providers in authentications controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,15 +1,25 @@
 class AuthenticationsController < Devise::OmniauthCallbacksController
+  
+  def provider
+    omni = request.env["omniauth.auth"]
+    raise omni
+  end
+  
+  alias_method :twitter, :provider
+  alias_method :linkedin, :provider
+  alias_method :facebook, :provider
+  
   def index
     @authentications = Authentication.all
   end
 
   def create
-    @authentication = Authentication.new(params[:authentication])
-    if @authentication.save
-      redirect_to authentications_url, :notice => "Successfully created authentication."
-    else
-      render :action => 'new'
-    end
+    #@authentication = Authentication.new(params[:authentication])
+    #if @authentication.save
+    #  redirect_to authentications_url, :notice => "Successfully created authentication."
+    #else
+    #  render :action => 'new'
+    #end
   end
 
   def destroy
@@ -17,16 +27,5 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
     @authentication.destroy
     redirect_to authentications_url, :notice => "Successfully destroyed authentication."
   end
-  
-  def linkedin
-    raise omni = request.env["omniauth.auth"].to_yaml
-  end
-  
-  def twitter
-    raise omni = request.env["omniauth.auth"].to_yaml
-  end
-  
-  def facebook
-    raise omni = request.env["omniauth.auth"].to_yaml
-  end
+
 end
