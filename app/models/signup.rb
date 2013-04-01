f99995ba0e78dc67591a918c952e5d622878b4d1
nbhartiya <neeharika@alum.mit.edu>
2013-04-01 14:29:52 -0700

    validates presence of, uniqueness of, format of emal addresses

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,3 +1,9 @@
 class Signup < ActiveRecord::Base
   attr_accessible :email, :chef
+  
+  # Makeshift email validation for now
+  validates :email, :presence => true,
+    :length => {:minimum => 3, :maximum => 254},
+    :uniqueness => true,
+    :format => /@/
 end
