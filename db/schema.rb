a7b1a96c3e47039bb635ed9522524091e45ca7ca
nbhartiya <neeharika@alum.mit.edu>
2013-04-05 01:40:04 -0700

    moved tony and jonny jpg, added a default of false to the approved column for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401161245) do
+ActiveRecord::Schema.define(:version => 20130404225316) do
 
   create_table "accreditations", :force => true do |t|
     t.string   "individual_income"
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(:version => 20130401161245) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "kind"
-    t.boolean  "approved"
+    t.boolean  "approved",               :default => false
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.string   "email",                  :default => "",    :null => false
