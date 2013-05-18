785897156e5e857f71411fcffa5802402e1b5af7
nbhartiya <neeharika@alum.mit.edu>
2013-05-17 18:11:22 -0700

    hid pages we dont want to show yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
   
   skip_before_filter :authenticate_user!
   
-  def index
-  end
 
   def howitworks
   end
@@ -11,13 +9,8 @@ class HomeController < ApplicationController
   def teamandjobs
   end
 
-  def tos
-  end
-
   def contactus
   end
 
-  def blog
-  end
 
 end
