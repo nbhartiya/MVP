e1a28d831a5ec322218dac6cf469a8aa0295af8f
nbhartiya <neeharika@alum.mit.edu>
2013-06-24 15:40:24 -0700

    added fields to survey questions for foodie survey

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -2,12 +2,16 @@
 #
 # Table name: survey_questions
 #
-#  id         :integer          not null, primary key
-#  kind       :string(255)
-#  question   :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  topic      :string(255)
+#  id           :integer          not null, primary key
+#  kind         :string(255)
+#  question     :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  topic        :string(255)
+#  low_extreme  :text
+#  high_extreme :text
+#  format       :string(255)
+#  active       :boolean
 #
 
 class SurveyQuestion < ActiveRecord::Base 
