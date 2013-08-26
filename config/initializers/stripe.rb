4c18a1d8194fad6deaa15998be6eca6cad942663
nbhartiya <neeharika@alum.mit.edu>
2013-08-26 12:56:54 -0700

    hooked up live stripe keys and made stripe live

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,7 @@
-Stripe.api_key = "sk_test_JmzVWvq3XPy3ILxJJrGg8Sao"
-STRIPE_PUBLISHABLE_KEY = "pk_test_XfS3clm6Vu4Uhyne1zheh4lV"
+if Rails.env.development? OR Rails.env.test?
+  Stripe.api_key = "sk_test_JmzVWvq3XPy3ILxJJrGg8Sao"
+  STRIPE_PUBLISHABLE_KEY = "pk_test_XfS3clm6Vu4Uhyne1zheh4lV"
+else
+  Stripe.api_key = "sk_live_hKn3GQGUxwfSp1zU6fO8FW2X"
+  STRIPE_PUBLISHABLE_KEY = "pk_live_GMpAJtBVaEM89F4af1CHfLTy"
+end
