fd30e101cec5d04dc7fd8d34e5f277946972cbe0
Wendy Lin <email.wendylin@gmail.com>
2013-08-26 19:17:58 -0700

    twitter message changed, made add to calendar on hover just in case requires multiple clicks

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,7 +4,7 @@ class NotificationMailer < ActionMailer::Base
   
   default :from => "neeharika@simmr.co"
 
-  def signup_foodie_email(user)
+  def signup_foodie_email_test(user)
     @user = user
     mail( :to => user.email,
     :subject => "Welcome to Simmr, #{user.first_name} #{user.last_name}!")
@@ -25,6 +25,6 @@ class NotificationMailer < ActionMailer::Base
   def waitlist_chef_email(signup)
     @signup = signup
     mail( :to => signup.email,
-    :subject => "You're on Your Way to Sharing Your Food with the World")
+    :subject => "You're on Your Way to Sharing Your Food with the World!")
   end
 end
