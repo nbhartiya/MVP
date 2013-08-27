68bc943dc7b9b7932e7092e39c353b6538f333ea
Wendy Lin <email.wendylin@gmail.com>
2013-08-27 14:49:05 -0700

    I think this resolves the email issues

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -15,4 +15,17 @@ class NotificationMailer < ActionMailer::Base
     mail( :to => user.email,
     :subject => "Welcome to Simmr, #{user.first_name} #{user.last_name}!")
   end
+
+  def waitlist_email(signup)
+    @signup = signup
+    mail( :to => signup.email,
+    :subject => "You're on Your Way to Some of the Best Meals of Your Life!")
+  end
+  
+  def waitlist_chef_email(signup)
+    @signup = signup
+    mail( :to => signup.email,
+    :subject => "You're on Your Way to Sharing Your Food with the World")
+  end
+  
 end
