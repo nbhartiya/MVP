a6fee2dc7b4fabcafaf0fe2231db76361a452782
Wendy Lin <email.wendylin@gmail.com>
2013-10-07 17:55:36 -0700

    foray into event purchase email, fixed some errors that I caused today...

--- app/mailers/mandrill_mailer.rb
+++ app/mailers/mandrill_mailer.rb
@@ -120,4 +120,67 @@ class MandrillMailer
     sending = m.messages.send_template template_name, template_content, message 
   end
 
+
+  def self.event_purchase_email(user, event)
+    @user = user
+    @event = event
+    require 'mandrill'
+    m = Mandrill::API.new(ENV["MANDRILL_KEY"])
+      message = {
+        :subject=> "#{user.first_name}, you're going to #{event.title} with #{event.host}!",
+        :from_name=> "Simmr Concierge",
+        :from_email => "neeharika@simmr.co",
+        :text=>"#{user.first_name}, you're going to #{event.title} with #{event.host}!
+        We're glad you can make it. Now all you have to do is show up and give your name at the door.
+        Eating is best in the company of good friends, so spread the word and bring friends!
+        Your event details: www.simmr.co/events/#{event.id}
+        Get excited!
+        Neeharika and Wendy
+        http://www.simmr.co
+        P.S. As always, please don't hesitate to reach out to us directly by replying to this email.",
+        # below are global merge vars. the name is the *|MERGE|* tag
+        #{}"global_merge_vars"=>[],
+        :to=>[
+          {
+            :email=> "linwendy08@gmail.com",
+            :name=> @user.first_name
+          }
+      ],
+      :from_email=>"neeharika@simmr.co"
+      }
+    template_name = "welcome-email"
+    template_content = [
+      {
+        :name => "title",
+        :content => "#{user.first_name}, you're going to #{event.title} with #{event.host}!"
+      },
+      {
+        :name => "std_preheader_content",
+        :content => "<p>#{user.first_name}, you're going to #{event.title} with #{event.host}!</p>"
+      },
+      {
+        :name => "image",
+        :content => "<img src = 'http://www.simmr.co/assets/launch_page_mock_done.jpg' style = 'max-width:550px;' id = 'headerImage campaign-icon'>"
+      },
+      {
+        :name => "content",
+        :content => "<h1>#{user.first_name}, you're going to #{event.title} with #{event.host}!</h1><p> We're glad you can make it. Now all you have to do is show up and give your name at the door. 
+        <br><p>Eating is best in the company of good friends, so spread the word and bring friends!<br><a href = 'http://www.simmr.co/events/#{event.id}>Your Event Details</a><br><br>Get excited! <br>Neeharika and Wendy<br><a href = 'http://www.simmr.co'>simmr</a>
+        <br><br>P.S. As always, please don't hesitate to reach out to us directly by replying to this email.</p>"
+      },
+      {
+        :name => "social",
+        :content => "<a href='http://www.facebook.com/simmrco'>like on Facebook </a> | <a href='https://twitter.com/simmrco'>follow on Twitter</a> | <a href = 'http://simmrco.wordpress.com'>check out our blog</a>"
+      },
+      { variablevariablevariable 
+        :name => "footer-content",
+        :content => "You're receiving this because you just bought a ticket to this event!"
+        },
+      {
+        :name => "utility",
+        :content => "<a href='*|UNSUB:http://www.simmr.co/home/unsubscribe|*'>unsubscribe from this list</a>"
+      }]
+    sending = m.messages.send_template template_name, template_content, message 
+  end
+
 end
