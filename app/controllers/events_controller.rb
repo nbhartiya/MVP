1ac3a8b932d84c44de36046690c7505dfb4879c4
nbhartiya <neeharika@alum.mit.edu>
2013-10-31 13:24:24 -0700

    making upcomign events visible to us

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,7 +37,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     if @event.approved == false
       if user_signed_in?
-        if @event.host.id==current_user.id
+        if @event.host.id==current_user.id || current_user.id==1 || current_user.id==13
           respond_to do |format|
             format.html
             format.json { render json: @event }
