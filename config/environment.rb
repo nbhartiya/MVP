c414e1012bb4d25391bab505f063bc51b4eb6e77
nbhartiya <neeharika@alum.mit.edu>
2014-01-31 18:24:42 -0800

    my attempt at fixing actionmailer ssl mismatch error

--- config/environment.rb
+++ config/environment.rb
@@ -12,5 +12,6 @@ ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
-  :enable_starttls_auto => true
+  :enable_starttls_auto => true,
+  :openssl_verify_mode => 'none'
 }
